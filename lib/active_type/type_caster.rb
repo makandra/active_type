73b9bfd02984ec84414bb2d7cae09a3691dd19bd
Henning Koch <henning.koch@makandra.de>
2014-09-23 11:33:33 +0200

    clean up

--- lib/active_type/type_caster.rb
+++ lib/active_type/type_caster.rb
@@ -2,80 +2,76 @@ module ActiveType
   class TypeCaster
 
     def self.get(connection, sql_type)
-      implementation = ActiveRecord::VERSION::STRING < '4.2' ? DelegateToColumn : DelegateToType
-      implementation.new(connection, sql_type)
+      native_caster = if ActiveRecord::VERSION::STRING < '4.2'
+        NativeCasters::DelegateToColumn.new(sql_type)
+      else
+        NativeCasters::DelegateToType.new(sql_type, connection)
+      end
+      new(sql_type, native_caster)
     end
 
-    class Base
+    def initialize(sql_type, native_caster)
+      @sql_type = sql_type
+      @native_caster = native_caster
+    end
 
-      def initialize(sql_type)
-        @sql_type = sql_type
-      end
-
-      def type_cast_from_user(value)
-        # For some reason, Rails defines additional type casting logic
-        # outside the classes that have that responsibility.
-        case @sql_type
-        when :integer
-          case value
-          when FalseClass
-            0
-          when TrueClass
-            1
-          when "", nil
-            nil
-          else
-            value.to_i
-          end
-        when :timestamp, :datetime
-          if ActiveRecord::Base.time_zone_aware_attributes
-            time = delegated_type_cast_from_user(value)
-            if time
-              ActiveSupport::TimeWithZone.new(nil, Time.zone, time)
-            else
-              time
-            end
-          else
-            delegated_type_cast_from_user(value)
-          end
-        when nil
-          value
+    def type_cast_from_user(value)
+      # For some reason, Rails defines additional type casting logic
+      # outside the classes that have that responsibility.
+      case @sql_type
+      when :integer
+        if value == ''
+          nil
         else
-          delegated_type_cast_from_user(value)
+          native_type_cast_from_user(value)
         end
+      when :timestamp, :datetime
+        time = native_type_cast_from_user(value)
+        if time && ActiveRecord::Base.time_zone_aware_attributes
+          time = ActiveSupport::TimeWithZone.new(nil, Time.zone, time)
+        end
+        time
+      else
+        native_type_cast_from_user(value)
       end
-
-      def delegated_type_cast_from_user(value)
-        raise "implement me"
-      end
-
     end
 
-    class DelegateToColumn < Base
-
-      def initialize(_connection, sql_type)
-        super(sql_type)
-        @column = ActiveRecord::ConnectionAdapters::Column.new('foo', nil, sql_type)
-      end
-
-      def delegated_type_cast_from_user(value)
-        @column.type_cast(value)
-      end
-
+    def native_type_cast_from_user(value)
+      @native_caster.type_cast_from_user(value)
     end
 
-    class DelegateToType < Base
+    module NativeCasters
 
-      def initialize(connection, sql_type)
-        super(sql_type)
-        @rails_cast_type = connection.lookup_cast_type(sql_type)
+      # Adapter for Rails 3.0 - 4.1.
+      # In these versions, casting logic lives in ActiveRecord::ConnectionAdapters::Colum
+      class DelegateToColumn
+
+        def initialize(sql_type)
+          @column = ActiveRecord::ConnectionAdapters::Column.new('foo', nil, sql_type)
+        end
+
+        def type_cast_from_user(value)
+          @column.type_cast(value)
+        end
+
       end
 
-      def delegated_type_cast_from_user(value)
-        @rails_cast_type.type_cast_from_user(value)
+      # Adapter for Rails 4.2+.
+      # In these versions, casting logic lives in subclasses of ActiveRecord::Type::Value
+      class DelegateToType
+
+        def initialize(sql_type, connection)
+          @active_record_type = connection.lookup_cast_type(sql_type)
+        end
+
+        def type_cast_from_user(value)
+          @active_record_type.type_cast_from_user(value)
+        end
+
       end
 
     end
 
   end
+
 end
