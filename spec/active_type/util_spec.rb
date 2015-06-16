ac12524d2ffb367f9d1ebd1fb5ec2e0ba8a17a6b
Henning Koch <henning.koch@makandra.de>
2015-06-16 14:00:00 +0200

    Backport spec to Ruby 1.8.7

--- spec/active_type/util_spec.rb
+++ spec/active_type/util_spec.rb
@@ -39,8 +39,8 @@ describe ActiveType::Util do
     describe 'for a relation' do
     
       it 'casts a scope to a scope of another class' do
-        record = UtilSpec::BaseRecord.create!(persisted_string: 'foo')
-        base_scope = UtilSpec::BaseRecord.where(persisted_string: 'foo')
+        record = UtilSpec::BaseRecord.create!(:persisted_string => 'foo')
+        base_scope = UtilSpec::BaseRecord.where(:persisted_string => 'foo')
         casted_scope = ActiveType::Util.cast(base_scope, UtilSpec::ExtendedRecord)
         casted_scope.build.should be_a(UtilSpec::ExtendedRecord)
         found_record = casted_scope.find(record.id)
@@ -49,9 +49,9 @@ describe ActiveType::Util do
       end
 
       it 'preserves existing scope conditions' do
-        match = UtilSpec::BaseRecord.create!(persisted_string: 'foo')
-        no_match = UtilSpec::BaseRecord.create!(persisted_string: 'bar')
-        base_scope = UtilSpec::BaseRecord.where(persisted_string: 'foo')
+        match = UtilSpec::BaseRecord.create!(:persisted_string => 'foo')
+        no_match = UtilSpec::BaseRecord.create!(:persisted_string => 'bar')
+        base_scope = UtilSpec::BaseRecord.where(:persisted_string => 'foo')
         casted_scope = ActiveType::Util.cast(base_scope, UtilSpec::ExtendedRecord)
         casted_match = UtilSpec::ExtendedRecord.find(match.id)
         casted_scope.to_a.should == [casted_match]
@@ -62,7 +62,7 @@ describe ActiveType::Util do
     describe 'for a record type' do
 
       it 'casts a base record to an extended record' do
-        base_record = UtilSpec::BaseRecord.create!(persisted_string: 'foo')
+        base_record = UtilSpec::BaseRecord.create!(:persisted_string => 'foo')
         extended_record = ActiveType::Util.cast(base_record, UtilSpec::ExtendedRecord)
         extended_record.should be_a(UtilSpec::ExtendedRecord)
         extended_record.should be_persisted
@@ -72,7 +72,7 @@ describe ActiveType::Util do
       end
 
       it 'casts an extended record to a base record' do
-        extended_record = UtilSpec::ExtendedRecord.create!(persisted_string: 'foo')
+        extended_record = UtilSpec::ExtendedRecord.create!(:persisted_string => 'foo')
         base_record = ActiveType::Util.cast(extended_record, UtilSpec::BaseRecord)
         base_record.should be_a(UtilSpec::BaseRecord)
         base_record.should be_persisted
@@ -82,33 +82,33 @@ describe ActiveType::Util do
       end
 
       it 'calls after_initialize callbacks of the cast target' do
-        base_record = UtilSpec::BaseRecord.create!(persisted_string: 'foo')
+        base_record = UtilSpec::BaseRecord.create!(:persisted_string => 'foo')
         extended_record = ActiveType::Util.cast(base_record, UtilSpec::ExtendedRecord)
         extended_record.virtual_string.should be_present
       end
 
       it 'lets after_initialize callbacks access attributes (bug in ActiveRecord#becomes)' do
-        base_record = UtilSpec::BaseRecord.create!(persisted_string: 'foo')
+        base_record = UtilSpec::BaseRecord.create!(:persisted_string => 'foo')
         extended_record = ActiveType::Util.cast(base_record, UtilSpec::ExtendedRecord)
         extended_record.virtual_string.should == 'persisted_string is foo'
       end
 
       it 'preserves the #type of an STI record that is casted to an ExtendedRecord' do
-        child_record = UtilSpec::Child.create!(persisted_string: 'foo')
+        child_record = UtilSpec::Child.create!(:persisted_string => 'foo')
         extended_child_record = ActiveType::Util.cast(child_record, UtilSpec::ExtendedChild)
         extended_child_record.should be_a(UtilSpec::ExtendedChild)
         extended_child_record.type.should == 'UtilSpec::Child'
       end
 
       it 'changes the #type of an STI record when casted to another type in the hierarchy' do
-        child_record = UtilSpec::Child.create!(persisted_string: 'foo')
+        child_record = UtilSpec::Child.create!(:persisted_string => 'foo')
         child_sibling_record = ActiveType::Util.cast(child_record, UtilSpec::ChildSibling)
         child_sibling_record.should be_a(UtilSpec::ChildSibling)
         child_sibling_record.type.should == 'UtilSpec::Child'
       end
 
       it 'preserves dirty tracking flags' do
-        base_record = UtilSpec::BaseRecord.create!(persisted_string: 'foo')
+        base_record = UtilSpec::BaseRecord.create!(:persisted_string => 'foo')
         base_record.changes.should == {}
         base_record.persisted_string = 'bar'
         base_record.changes.should == { 'persisted_string' => ['foo', 'bar'] }
