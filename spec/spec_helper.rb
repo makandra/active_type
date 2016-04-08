d73b375ee28b23c467cb91f9919b14194454f460
Tobias Kraze <tobias.kraze@makandra.de>
2016-04-08 14:11:52 +0200

    suppress AR deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ $: << File.join(File.dirname(__FILE__), "/../../lib" )
 require 'active_type'
 
 ActiveRecord::Base.default_timezone = :local
+ActiveRecord::Base.raise_in_transactional_callbacks = true if ActiveRecord::Base.respond_to?(:raise_in_transactional_callbacks)
 
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].each {|f| require f}
 Dir["#{File.dirname(__FILE__)}/shared_examples/*.rb"].each {|f| require f}
