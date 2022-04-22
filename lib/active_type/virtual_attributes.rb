285591c47cd2f20c698ee66df6a02350f857d916
Niklas Häusele <niklas.haeusele@makandra.de>
2022-04-22 11:28:25 +0200

    Use to_formatted_s instead of to_s with args
    https://github.com/rails/rails/blob/7-0-stable/activesupport/CHANGELOG.md#rails-700rc1-december-06-2021
    Fixes #157

--- lib/active_type/virtual_attributes.rb
+++ lib/active_type/virtual_attributes.rb
@@ -273,7 +273,7 @@ module ActiveType
       if value.is_a?(String) && value.length > 50
         "#{value[0, 50]}...".inspect
       elsif value.is_a?(Date) || value.is_a?(Time)
-        %("#{value.to_s(:db)}")
+        %("#{value.to_formatted_s(:db)}")
       elsif value.is_a?(Array) && value.size > 10
         inspected = value.first(10).inspect
         %(#{inspected[0...-1]}, ...])
