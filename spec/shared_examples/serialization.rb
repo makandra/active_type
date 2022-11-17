04cee41cde436dbf91fdf0a8b4355e2fba71b696
Tobias Kraze <tobias.kraze@makandra.de>
2022-11-17 10:34:21 +0100

    deal with psych 4 in tests

--- spec/shared_examples/serialization.rb
+++ spec/shared_examples/serialization.rb
@@ -1,6 +1,6 @@
 shared_examples_for "an instance supporting serialization" do
   it 'serializes correctly with to_yaml' do
-    deserialized = YAML.load(subject.to_yaml)
+    deserialized = YAML.respond_to?(:unsafe_load) ? YAML.unsafe_load(subject.to_yaml) : YAML.load(subject.to_yaml)
 
     subject.attributes.each do |attr, value|
       expect(deserialized.send(attr)).to eq(value)
